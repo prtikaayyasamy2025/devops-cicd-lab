183e6913b292bb7761b9f6e2c83efe370b99e47f
prtikaayyasamy2025 <prtikaayyasamy2025@gmail.com>
2026-04-17 11:38:15 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello DevOps!'));
+
+app.get('/health', (req, res) => res.status(200).send('OK'));
+
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(3000, () => console.log('Running on port 3000'));
+}
